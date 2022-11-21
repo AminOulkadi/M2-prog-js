3b08efec380b84bf624282e373c676b294614f84
Amin <34860@ma-web.nl>
2022-11-21 10:51:48 +0100

    jvopdracht

---
+++ les1/app.js
@@ -0,0 +1,25 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!")
+        //code gaat hier!
+        let appNaam = "joseph";
+        let versienummer = 0.9;
+        let versiedatum = 21/11/2022;
+        let autheur = "Amin";
+        let copyright = "this code belongs to joe.";
+        let distributeur = "joe mama industries";
+        let darkmode = true;
+        console.log(appNaam)
+        console.log(versienummer)
+        console.log(versiedatum)
+        console.log(autheur)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log(darkmode)
+    }
+}
+
+let app = new App();
+app.runApplication();
